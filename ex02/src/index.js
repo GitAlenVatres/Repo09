9a9b86052c84a1b942578b45300599dad13f6b75
GitAlenVatres <90286976+GitAlenVatres@users.noreply.github.com>
2021-09-22 10:21:29 +0200

    Add files via upload

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+function myFunction (myParameter) {
+if (myParameter == true) {
+return "The parameter is true!";
+}
+if (myParameter == false){
+return "The parameter is false!";
+}
+// Only change code above this line
+module.exports = myFunction;

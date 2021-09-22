f5f0f9823e00084656f3f45e8a737b38a5fc66de
GitAlenVatres <90286976+GitAlenVatres@users.noreply.github.com>
2021-09-22 10:28:43 +0200

    Add files via upload

---
+++ ex14/src/index.js
@@ -0,0 +1,16 @@
+ // Only change code below this line
+function myForLoop1 () {
+for (var i = 0;i<10;i++2) {
+    console.log (i);
+    }
+}
+function myForLoop2 () {
+for (var j = 1;i<10;i++2) {
+    console.log (j);
+    }
+}
+// Only change code above this line
+module.exports = {
+myForLoop1;
+myForLoop2
+};
